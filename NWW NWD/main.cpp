74ad8b1dc569a45e724957e102d59277397c707c
GrubszyGruby <wwesk@o2.pl>
2022-05-31 20:10:49 +0200

    kod

---
+++ NWW NWD/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+int nwd();
+int nww();
+
+int main() {
+	nww();
+}
+
+int nwd() {
+	int a,b;
+
+	cout << "Podaj a: ";
+	cin >> a;
+
+	cout << "Podaj b: ";
+	cin >> b;
+
+
+	while(a!=b) {
+		if(a<b) {
+			b=b-a;
+		}
+		else {
+			a=a-b;
+		}
+	}
+	cout << "NWD: " << a << endl;
+
+	return 0;
+}
+
+int nww() {
+	int a,b;
+
+	cout << "Podaj a: ";
+	cin >> a;
+
+	cout << "Podaj b: ";
+	cin >> b;
+
+	int iloczyn = a*b;
+
+	while(a!=b) {
+		if(a<b) {
+			b=b-a;
+		}
+		else {
+			a=a-b;
+		}
+	}
+
+	int nww = iloczyn/a;
+
+	cout << "NWW: " << nww << endl;
+
+	return 0;
+}
+
+
