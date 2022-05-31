53eefcfaf8bc62037d29bb74df030ca7fb5b89f3
GrubszyGruby <wwesk@o2.pl>
2022-05-31 20:12:41 +0200

    kod

---
+++ Liczbypierwsze/LiczbyPierwsze.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int do_ktorej;
+
+    cout << "Podaj do ktorej liczby chcesz sprawdzic: ";
+    cin >> do_ktorej;
+    do_ktorej = 0;
+
+        for (int i = 1; i <= do_ktorej; i++)
+        {
+            if (i % 1 == 0 && i % i == 0)
+                cout << "Liczba jest liczba pierwsza.";
+                cout << i;
+        }
+
+
+
+    return 0;
+}
